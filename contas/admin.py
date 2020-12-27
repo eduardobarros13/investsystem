1c4f8db6ac50002021a45d16fe0f3e52db6a5bf7
eduardobarros13 <eduardobarros.eng@outlook.com>
2020-12-27 12:58:12 -0300

    add model evolucao

--- contas/admin.py
+++ contas/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import Categoria
 from .models import Transacao
 from .models import Trade
+from .models import Evolucao
 
 
-admin.site.register(Categoria)
-admin.site.register(Transacao)
 admin.site.register(Trade)
+admin.site.register(Evolucao)
